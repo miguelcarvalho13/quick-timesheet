b5293c9161ff9ba687868ce7d7ebe9297a3ae742
Miguel Carvalho <macs.11.12.13@gmail.com>
2022-05-02 16:22:50 -0300

    feat: implement validation format

---
+++ src/formatters/Validator.ts
@@ -0,0 +1,12 @@
+import { ValidationTypes } from "../validators/TimesheetDayValidator";
+
+export function formatValidation(validation: ValidationTypes): string {
+  switch (validation) {
+    case ValidationTypes.DUPLICATED_DATE:
+      return 'There are other dates with the same date as this one';
+    case ValidationTypes.UNORDERED_TIME_INTERVALS:
+      return 'The time intervals are out of order';
+    default:
+      return '';
+  }
+}
