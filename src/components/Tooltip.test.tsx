42327fe16c8ded4524c478079ac89923d944ff7c
Miguel Carvalho <macs.11.12.13@gmail.com>
2022-05-02 14:37:56 -0300

    feat: add tooltip component

---
+++ src/components/Tooltip.test.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Tooltip from './Tooltip';
+
+const setup = () => {
+  return render(
+    <>
+      <div data-testid='click-outside'>Click outside</div>
+      <Tooltip button={'my tooltip button'}>
+        <span>My tooltip content</span>
+      </Tooltip>
+    </>
+  )
+}
+
+// TODO: find a way to test the outcome of tailwind classes in this tests instead of the presence of the class
+test('renders the tooltip with a button', () => {
+  setup();
+  expect(screen.getByRole('button')).toHaveTextContent(/^my tooltip button$/);
+});
+
+test('renders the tooltip initially hidden', () => {
+  setup();
+  const tooltip = screen.getByRole('tooltip', { hidden: true });
+  expect(tooltip).toHaveClass('invisible');
+  expect(tooltip).toHaveTextContent(/^My tooltip content$/);
+});
+
+test('opens the tooltip when hovering on it and closes when unhovering', async () => {
+  setup();
+  const tooltip = screen.getByRole('tooltip', { hidden: true });
+  await userEvent.hover(screen.getByRole('button'));
+  expect(tooltip).not.toHaveClass('invisible');
+  await userEvent.unhover(screen.getByRole('button'));
+  expect(tooltip).toHaveClass('invisible');
+});
+
+test('opens the tooltip when clicking on it and closes clicking outside', async () => {
+  setup();
+  const tooltip = screen.getByRole('tooltip', { hidden: true });
+
+  await userEvent.click(screen.getByRole('button'));
+  expect(tooltip).not.toHaveClass('invisible');
+
+  await userEvent.unhover(screen.getByRole('button'));
+  // if it was open through a click it should remain open while unhovering it
+  expect(tooltip).not.toHaveClass('invisible');
+
+  await userEvent.click(screen.getByTestId('click-outside'));
+  expect(tooltip).toHaveClass('invisible');
+});
+
+test('opens the tooltip through tab focus and closes it on the next tab', async () => {
+  setup();
+  const tooltip = screen.getByRole('tooltip', { hidden: true });
+  await userEvent.keyboard('{Tab}');
+  expect(tooltip).not.toHaveClass('invisible');
+  await userEvent.keyboard('{Tab}');
+  expect(tooltip).toHaveClass('invisible');
+});
---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,52 @@
+import React, { SyntheticEvent, useState } from "react";
+
+interface TooltipProps {
+  button: React.ReactNode,
+  children: React.ReactNode
+}
+
+function Tooltip({ button, children }: TooltipProps) {
+  const [isTooltipOpen, setTooltipOpen] = useState(false);
+  const [isTooltipOpenThroughClick, setTooltipOpenThroughClick] = useState(false);
+
+  function openTooltip(event: SyntheticEvent): void {
+    setTooltipOpen(true);
+
+    if (['focus', 'click'].includes(event.type)) {
+      setTooltipOpenThroughClick(true);
+    }
+  }
+
+  function closeTooltip(): void {
+    setTooltipOpen(false);
+    setTooltipOpenThroughClick(false);
+  }
+
+  function closeTooltipOnMouseLeave(): void {
+    if (!isTooltipOpenThroughClick) {
+      setTooltipOpen(false);
+    }
+  }
+
+  return (
+    <>
+      <button
+        onMouseEnter={openTooltip}
+        onMouseLeave={closeTooltipOnMouseLeave}
+        onClick={openTooltip}
+        onBlur={closeTooltip}
+        onFocus={openTooltip}
+      >
+        {button}
+      </button>
+      <div
+        className={`absolute bg-sky-700 font-thin px-3 py-2 rounded-lg shadow-md text-sm transition-opacity z-50 ${isTooltipOpen ? 'opacity-100' : 'invisible opacity-0'}`}
+        role='tooltip'
+      >
+        {children}
+      </div>
+    </>
+  );
+}
+
+export default Tooltip;
