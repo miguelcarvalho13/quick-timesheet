c5f95849c7769cba5b231ab3c73c25150dca993f
Miguel Carvalho <macs.11.12.13@gmail.com>
2022-05-05 10:33:23 -0300

    chore: bring textarea to be inside the label
    So that it is easier to deal with layout changes.

--- src/App.tsx
+++ src/App.tsx
@@ -14,27 +14,22 @@ function App() {
   return (
     <div className="bg-sky-900 font-sans min-h-screen selection:bg-sky-500 text-sky-50">
       <form
-        className="content-center flex flex-col items-center justify-center p-8 space-y-2"
+        className="content-center flex flex-col items-center justify-center p-8 space-y-10"
         onSubmit={convertText}
       >
-        <label
-          className="flex-none font-medium text-left text-sm w-2/3"
-          htmlFor="text-to-convert"
-        >
+        <label className="flex flex-col flex-none font-medium text-left text-sm w-2/3">
           Text to convert
+          <textarea
+            className="bg-sky-700 flex-auto min-h-[16rem] focus:outline focus:outline-2 mt-2 outline-sky-200 px-2 py-1 resize-y rounded-sm shadow-md transition-all duration-75 ease-in-out"
+            value={textValue}
+            onChange={(e) => setTextValue(e.target.value)}
+          ></textarea>
         </label>
-        <textarea
-          id="text-to-convert"
-          className="bg-sky-700 flex-auto min-h-[16rem] focus:outline focus:outline-2 outline-sky-200 px-2 py-1 resize-y rounded-sm shadow-md transition-all duration-75 ease-in-out w-2/3"
-          value={textValue}
-          onChange={(e) => setTextValue(e.target.value)}
-        ></textarea>
-        <br />
         <input
           className="active:bg-sky-600 bg-sky-500 cursor-pointer flex-none font-medium hover:bg-sky-400 px-8 py-3 rounded-md shadow-md w-fit"
           type="submit"
           value="Convert"
-        ></input>
+        />
       </form>
       <div className="p-4">
         <TimesheetTable daysList={daysList} />
